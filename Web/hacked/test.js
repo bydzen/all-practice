ff2faebdf499598d755ae7ff4873e82fb4bbf16b
Sultan Kautsar <42274355+bydzen@users.noreply.github.com>
2021-12-21 08:44:25 +0700

    Add hacked folder

---
+++ Web/hacked/test.js
@@ -0,0 +1,8 @@
+// create an array of animals
+var animals = ["dog", "cat", "bird", "fish", "lizard", "snake", "turtle", "hamster", "gerbil", "pig", "cow", "chicken", "duck", "goat", "horse", "sheep", "penguin", "elephant", "monkey", "panda", "koala", "bear", "deer", "beaver", "camel", "dolphin", "giraffe", "rhinoceros", "hippo", "kangaroo", "lion", "panther", "parrot", "rabbit", "seal", "shark", "tiger", "wolf", "zebra"];
+
+// sort the array alphabetically
+animals.sort();
+
+// print the array to the console
+console.log(animals);
